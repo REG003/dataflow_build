c7f18c7e0ca99acd45f3bc59eb2f7e3f9e148b73
REG003 <94829317+REG003@users.noreply.github.com>
2022-09-09 22:43:12 +1000

    Update git_dagfile_load.sh

--- scripts/git_dagfile_load.sh
+++ scripts/git_dagfile_load.sh
@@ -2,4 +2,4 @@ projectid=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 dagpath=$1
 # copying scripts for cloud composer in dags folder
 
-gsutil cp dags_main.py ${dagpath}
+gsutil cp composer-dataflow-dag.py ${dagpath}
